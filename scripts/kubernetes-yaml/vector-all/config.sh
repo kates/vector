1fda19abb973ca6e52a8307231c49ca6e52117fd
MOZGIII <mike-n@narod.ru>
2020-11-18 00:22:43 +0300

    Switch the generated Kubernetes YAML layout to multiple deployment variants
    Signed-off-by: MOZGIII <mike-n@narod.ru>

--- scripts/kubernetes-yaml/vector-all/config.sh
+++ scripts/kubernetes-yaml/vector-all/config.sh
@@ -2,4 +2,4 @@
 # shellcheck disable=SC2034
 CHART="distribution/helm/vector"
 RELEASE_NAME="vector"
-TARGET_FILE="distribution/kubernetes/vector.yaml"
+TARGET_FILE="distribution/kubernetes/vector-all/resources.yaml"
